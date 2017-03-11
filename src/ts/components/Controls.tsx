f69dc8fbc6353e0d5518145ac584f9f076e7e6ab
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-03-11 13:57:19 -0300

    Controls/ButtonWithShortcut

--- src/ts/components/Controls.tsx
+++ src/ts/components/Controls.tsx
@@ -15,16 +15,31 @@ export class Controls extends React.Component<ControlsProps, undefined> {
     return (
       <section className="controls-overlay">
         <nav className="patterns">
-          <button onClick={() => this.props.onPastePattern(Patterns.glider)}>Glider</button>
-          <button onClick={() => this.props.onPastePattern(Patterns.lightweightSpaceship)}>Lightweight Spaceship</button>
-          <button onClick={() => this.props.onPastePattern(Patterns.biClock)}>Bi-Clock</button>
+          <ButtonWithShortcut onClick={() => this.props.onPastePattern(Patterns.glider)} label="Glider" shortcut="1"/>
+          <ButtonWithShortcut onClick={() => this.props.onPastePattern(Patterns.lightweightSpaceship)} label="Lightweight Spaceship" shortcut="2"/>
+          <ButtonWithShortcut onClick={() => this.props.onPastePattern(Patterns.biClock)} label="Bi-Clock" shortcut="3"/>
         </nav>
         <nav className="controls">
-          <button onClick={this.props.onNextStep}>Next Step</button>
-          <button onClick={this.props.onStartStop}>{ this.props.isEvolving ? 'Pause' : 'Unpause'}</button>
-          <button onClick={this.props.onClear}>Clear</button>
+          <ButtonWithShortcut onClick={this.props.onNextStep} label="Next Step" shortcut="Enter"/>
+          <ButtonWithShortcut onClick={this.props.onStartStop} label={ this.props.isEvolving ? 'Pause' : 'Unpause'} shortcut="Space"/>
+          <ButtonWithShortcut onClick={this.props.onClear} label="Clear" shortcut="Delete"/>
         </nav>
       </section>
     );
   }
+}
+
+interface ButtonWithShortcutProps {
+  readonly label: string;
+  readonly shortcut: string;
+  readonly onClick: () => void;
+}
+
+function ButtonWithShortcut(props: ButtonWithShortcutProps) {
+  return (
+    <button onClick={props.onClick}>
+      <label>{props.label}</label>
+      <small>{props.shortcut}</small>
+    </button>
+  )
 }
